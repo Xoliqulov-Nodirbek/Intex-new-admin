b634b9233ba8c4351bfb72a23354d35ce8f3498b
IlkhamBurkhanov <2010053.ilkhomjon.burkhonov@gmail.com>
2022-11-22 00:58:35 +0500

    ,

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,6 @@ module.exports = {
       margin: {
         navBarIcon: "14px",
       },
-<<<<<<< HEAD
       padding:{
         headerInpPl:'38px',
         headerPaddingTopBottom:'19px',
@@ -34,30 +33,6 @@ module.exports = {
         logoLoginHeight:'22px',
         addProductHeight:'3px',
         orderModal:"700px"
-=======
-      padding: {
-        headerInpPl: "38px",
-        headerPaddingTopBottom: "19px",
-        headerPaddingX: "30px",
-        homeContentPadding: "30px",
-        selectInp: "17.5px",
-      },
-      width: {
-        logoWidth: "180px",
-        loginLogoWidth: "240px",
-        sitebarWidth: "253px",
-        supportWidth: "150px",
-        headerInputWidth: "260px",
-        homeInpWidth: "284px",
-        homeSortWidth: "170px",
-        addProductListWidth: "65%",
-        submitBtnsWidth: "331px",
-      },
-      height: {
-        logoHeight: "17px",
-        logoLoginHeight: "22px",
-        addProductHeight: "3px",
->>>>>>> 36fe53e86e97280c00fb0b917afcb8e7deb5ab41
       },
       backgroundColor: {
         loginBtn: "#2B3D90",
@@ -75,7 +50,6 @@ module.exports = {
         resetBtn: "#F2F2F2",
         submitBtnBg: "#2B3D91",
       },
-<<<<<<< HEAD
       colors:{
         addProductColor:"#24283A",
         addProductLinks:"#D8D8D8",
@@ -89,22 +63,6 @@ module.exports = {
         homeSortWrap:'#666',
         russuanColor:"#2B3D91",
         borderColor:"#E3E5E5"
-=======
-      colors: {
-        addProductColor: "#24283A",
-        addProductLinks: "#D8D8D8",
-        forgotPasswordColor: "#4F46E5",
-        inputPleacholderColor: "#B4B6B8",
-        ruUzUsColors: "#2B3D91",
-        navBarColor: "#464A4D",
-        navSubColor: "#B9B9B9",
-        supportColor: "#109EF4",
-        homeColor: "#333",
-        homeSortWrap: "#666",
-        russuanColor: "#2B3D91",
-        paginationColor: '#A6A8B1',
-        paginationButtonColor: '#666666'
->>>>>>> 36fe53e86e97280c00fb0b917afcb8e7deb5ab41
       },
       fontFamily: {
         inter: 'Inter'
