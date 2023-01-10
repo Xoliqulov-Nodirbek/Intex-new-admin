c0ecc6dc4a220243d93120d60a8c1fc640053e91
kholiqulov_nodirbek <99204049+Xoliqulov-Nodirbek@users.noreply.github.com>
2023-01-10 09:13:03 +0500

    Add userPage

---
+++ src/components/TBody/TBody.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { useState } from "react";
+import Dots from "../../Assets/Images/TableImgs/dots.svg";
+import Edit from "../../Assets/Images/TableImgs/edit.svg";
+import Delete from "../../Assets/Images/TableImgs/trash.svg";
+
+export default function TBody({ vitalData }) {
+  const [showModal, setShowModal] = useState(false);
+
+  // if (!showModal) return null;
+
+  const handleClick = (e) => {
+    if (e.target.id === "oram") setShowModal(false);
+  };
+  return (
+    <tbody className="bg-white">
+      <tr className="h-2.5 bg-[#E5E5E5]"></tr>
+      {vitalData.length > 0 &&
+        vitalData.map((el, i) => {
+          return (
+            <tr className="flex items-center border-t" key={i}>
+              <td className="w-11 flex justify-center">
+                <input className="w-4 h-4 cursor-pointer" type="checkbox" name={`input${i}`} />
+              </td>
+              {el.map((a, i) => {
+                return (
+                  <td className={`py-3 pl-3 ${a.style}`} key={i}>
+                    {a.image ? (
+                      <img
+                        className="w-6 h-6 rounded-full mr-[6px]"
+                        src="https://via.placeholder.com/42x38"
+                        alt="basseyn"
+                      />
+                    ) : null}
+                    <span className={`truncate text-base text-[#24283A] ${a.textClass}`}>
+                      {a.title}
+                    </span>
+                  </td>
+                );
+              })}
+              <td
+                onClick={() => setShowModal(true)}
+                className="rrelative flex justify-center flex-grow w-[95px] py-[17px] cursor-pointer"
+              >
+                <img src={Dots} alt="three dots" />
+                {showModal ? (
+                  <div
+                    id="oram"
+                    onClick={handleClick}
+                    className="absolute grid grid-cols-1 text-start border bg-white p-3 space-y-3 rounded-[5px] shadow-[0px_12px_23px_rgba(150, 150, 150, 0.1)]"
+                  >
+                    <button className="flex items-center">
+                      <img src={Edit} alt="" />
+                      Изменить
+                    </button>
+                    <button className="flex items-center">
+                      <img src={Delete} alt="" />
+                      Удалить
+                    </button>
+                  </div>
+                ) : (
+                  ""
+                )}
+              </td>
+            </tr>
+          );
+        })}
+    </tbody>
+  );
+}
---
+++ src/components/THead/THead.js
@@ -0,0 +1,23 @@
+import React from "react";
+import Sort from "../../Assets/Images/TableImgs/sort.svg";
+
+export default function THead({ data }) {
+  return (
+    <thead className="bg-[#f2f2f2]">
+      <tr className="flex items-center">
+        <th className="w-11 flex justify-center">
+          <input type="checkbox" />
+        </th>
+        {data.length &&
+          data.map((i, a) => {
+            return (
+              <th className={`${i.style} flex justify-start items-center py-3 pl-3`} key={a}>
+                <span className="text-sm text-[#464A4D] font-medium">{i.title}</span>
+                {i.image ? <img className="ml-2.5" src={Sort} alt="sort icon" /> : null}
+              </th>
+            );
+          })}
+      </tr>
+    </thead>
+  );
+}
