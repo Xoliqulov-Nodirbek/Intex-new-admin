7aa5d218ac6ca4b8803772ae4c273d22240ef1ec
nozimjonpro <nozimjonkeldiyev@yandex.ru>
2023-01-10 17:56:34 +0500

    Added edit and delete modal to table

--- src/components/TBody/TBody.js
+++ src/components/TBody/TBody.js
@@ -11,6 +11,17 @@ export default function TBody({ vitalData }) {
   const handleClick = (e) => {
     if (e.target.id === "oram") setShowModal(false);
   };
+
+  const handleModal = (e, i) => {
+    if (e.target.matches(`.edit_dots${i}`)) {
+      e.target.parentNode.classList.add("relative");
+      e.target.nextElementSibling.classList.remove("hidden");
+      e.target.nextElementSibling.classList.add("grid");
+      setShowModal(true);
+    } else {
+      e.target.nextElementSibling.classList.add("hidden");
+    }
+  };
   return (
     <tbody className="bg-white">
       <tr className="h-2.5 bg-[#E5E5E5]"></tr>
@@ -20,7 +31,7 @@ export default function TBody({ vitalData }) {
             <tr className="flex items-center border-t" key={i}>
               <td className="w-11 flex justify-center">
                 <input
-                  className="w-4 h-4 cursor-pointer"
+                  className="w-[18px] h-[18px] cursor-pointer"
                   type="checkbox"
                   name={`input${i}`}
                 />
@@ -37,8 +48,8 @@ export default function TBody({ vitalData }) {
                     ) : null}
                     <span
                       className={`${a.textClass} ${
-                        a?.label ? a?.label : "text-[#24283A]"
-                      } truncate text-base  `}
+                        a?.label ? a?.label : "text-[#24283A] text-sm"
+                      } truncate text-base  text-sm`}
                     >
                       {a.title}
                     </span>
@@ -46,28 +57,25 @@ export default function TBody({ vitalData }) {
                 );
               })}
               <td
-                onClick={() => setShowModal(true)}
-                className="relative flex justify-center flex-grow w-[95px] py-[17px] cursor-pointer"
+                className="flex  flex-grow w-[95px] py-[17px] cursor-pointer"
+                onClick={(e) => handleModal(e, i)}
               >
-                <img src={Dots} alt="three dots" />
-                {showModal ? (
-                  <div
-                    id="oram"
-                    onClick={handleClick}
-                    className="absolute grid grid-cols-1 text-start border bg-white p-3 space-y-3 rounded-[5px] shadow-[0px_12px_23px_rgba(150, 150, 150, 0.1)]"
-                  >
-                    <button className="flex items-center">
-                      <img src={Edit} alt="" />
-                      Изменить
-                    </button>
-                    <button className="flex items-center">
-                      <img src={Delete} alt="" />
-                      Удалить
-                    </button>
-                  </div>
-                ) : (
-                  ""
-                )}
+                <img className={`edit_dots${i}`} src={Dots} alt="three dots" />
+
+                <div
+                  id="oram"
+                  onClick={handleClick}
+                  className="absolute hidden right-0 top-0 grid-cols-1 text-start border bg-white p-1 space-y-1 rounded-[5px] shadow-[0px_12px_23px_rgba(150, 150, 150, 0.1)]"
+                >
+                  <button className="flex items-center text-xs">
+                    <img src={Edit} alt="" width={16} height={17} />
+                    Изменить
+                  </button>
+                  <button className="flex items-center text-xs">
+                    <img src={Delete} alt="" width={16} height={17} />
+                    Удалить
+                  </button>
+                </div>
               </td>
             </tr>
           );
